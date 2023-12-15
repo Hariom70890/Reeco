254d80cc875e0ced8e881aa7feac859d956ee03e
Hariom70890 <hariomsunilverma@gmail.com>
2023-12-15 12:50:56 +0530

    some part is remaining

---
+++ src/store/store.js
@@ -0,0 +1,9 @@
+import { applyMiddleware, combineReducers, legacy_createStore } from "redux";
+import rootReducer from "../redux/reducer/reducer";
+import thunk from "redux-thunk";
+
+const allReducer = combineReducers({
+   rootReducer,
+});
+
+export const store = legacy_createStore(allReducer, applyMiddleware(thunk));
